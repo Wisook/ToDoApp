ebe9c5cc934fd49f5cbffdf7c1d385dd11a694fa
Ville Kurkela <kurkela.ville@hotmail.com>
2021-03-21 00:31:08 +0200

    add

--- todo.js
+++ todo.js
@@ -1,31 +1,45 @@
+// Muuttujat
 var inputArea = document.querySelector("#inputValue");
 var btn = document.querySelector("button");
 var btnReset = document.querySelector("#reset");
 var btnLast = document.querySelector("#removeLast");
 var ulArea = document.querySelector("ol");
 
-btn.addEventListener('click', addItem);
+// Kuuntelijat
 btnReset.addEventListener('click', reset);
 btnLast.addEventListener('click', lastItem);
 
-
-function addItem() {
-  var getValue = inputArea.value;
-  var createElement = document.createElement("li");
-  createElement.textContent = getValue;
-  if(inputArea.value == "" || inputArea.value.length < 3) {
-    alert("Liian lyhyt tai tyhjä kenttä.");
-}
-else {
-    console.log("Toimii.")
-    ulArea.appendChild(createElement);
-}
-}
-
+// Funktiot
 function reset() {
   ulArea.innerHTML = "";
 }
 function lastItem() {
   ulArea.removeChild(ulArea.lastElementChild);
 }
+function myFunction() {
+  var item = document.getElementById("todoInput").value;
+  if (item == "" || item.length < 3) {
+    alert("Kirjoita jotakin tai liian lyhyt teksti.");
+  } else {
+    console.log("Toimii.")
+    var text = document.createTextNode(item);
+    var newItem = document.createElement("li");
+    var crap = document.getElementById("todoList");
+    crap.appendChild(newItem);
+    var checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.onclick = updateItem.bind(checkBox);
+    newItem.appendChild(checkBox);
+    newItem.appendChild(text);
+    document.getElementById("todoInput").value = "";
+    checkBox.className = "addedClass";
+  }
 
+  function updateItem() {
+    if (this.checked) {
+      this.parentNode.style.textDecoration = "line-through";
+    } else {
+      this.parentNode.style.textDecoration = "none";
+    }
+  }
+}
